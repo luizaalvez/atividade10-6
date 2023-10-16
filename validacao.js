70194591c8aa29a95eb503de352ade67a7fb8740
luizaalvez <139913790+luizaalvez@users.noreply.github.com>
2023-10-16 17:06:55 -0300

    Update validacao.js

--- validacao.js
+++ validacao.js
@@ -19,13 +19,13 @@ function validarFormulario() {
         return false;
     }
 
-    if (email.indexOf('@') === -1 || email.lastIndexOf('.') === -1) {
+    if (email.indexOf('@') === -1 || email.lastIndf('.') === -1) {
         alert('Email inválido.');
         return false;
     }
 
-    var dddsValidos = ['11', '12', '13', '14', '15', '16', '17', '18', '19'];
-    var ddd = telefone.substring(0, 2);
+    var dddsValidos = ['61','62','64','65','66','67','82','71','73','74','75','77','85','88','98','99','83','81','87','86','89','84','79','68','96','92','97','91','93','94','69','95','63','27','28','31','32','33','34','35','37','38','21','22','24','11','12','13','14',15,16,17,'18,19','41','42','43','44','45','46','51','53','54','55','47','48','49'];
+    var d = telefone.substring(0, 2);
     if (dddsValidos.indexOf(ddd) === -1) {
         alert('DDD de telefone inválido.');
         return false;
